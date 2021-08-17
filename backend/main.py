5af57bea957b00148b12e6500108ec316c905521
advay168 <advay168@gmail.com>
2021-08-17 16:25:29 +0530

    added Procfile

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=("http://localhost:3000",),
+    allow_origins=("http://localhost:3000", "https://learn-spanish.herokuapp.com"),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
