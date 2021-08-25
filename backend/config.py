eda456d7b4952f466f3a66846963e519aa7eb26d
advay168 <advay168@gmail.com>
2021-08-25 12:36:53 +0530

    fixed bug in db uri

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 import os
 
 DB_URI = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/learn-spanish")
+if DB_URI.startswith("postgres://"):
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
 
 CORS_ALLOWED_ORIGINS = (
     "http://localhost:3000",
