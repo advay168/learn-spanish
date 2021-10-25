ddbe1b63149f2e4b968572e73d9682678db58ed8
advay168 <advay168@gmail.com>
2021-10-25 20:33:05 +0530

    Added option to switch test type

--- src/components/Test/styles.ts
+++ src/components/Test/styles.ts
@@ -1,25 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
-
-export const Heading =styled.h3`
-    font-size:2rem;
-    text-align: center;
-    color: #924848;
-`;
-
-export const OptionsGrid = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 20px;
-    justify-items: center;
-    margin: 5px 20px;
-`;
-
-export const Option = styled.button`
-    /* background:green; */
-    width:100%;
-    max-width:500px;
-    height: 90px;
-    font-size: 20px;
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  margin-top: 40px;
+  button {
+    width: 30%;
+    height: 60px;
+  }
 `;
