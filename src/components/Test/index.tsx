370c21163d8b6cb090b9803d15570a42359476a4
advay168 <advay168@gmail.com>
2021-10-25 20:33:05 +0530

    Added option to switch test type

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Components
 import TestQuestion from "../TestQuestion";
 import TestConjugation from "../TestConjugation";
 
+//Styles
+import { Container } from "./styles";
+
 export default function Test() {
-  return <TestConjugation />;
-  return <TestQuestion />;
+  const [askMeaning, setAskMeaning] = useState(true);
+  const [chosen, setChosen] = useState(false);
+  if (!chosen) {
+    return (
+      <Container>
+        <button
+          onClick={() => {
+            setChosen(true);
+            setAskMeaning(false);
+          }}
+        >
+          Conjugation
+        </button>
+        <button
+          onClick={() => {
+            setChosen(true);
+            setAskMeaning(true);
+          }}
+        >
+          Meanings
+        </button>
+      </Container>
+    );
+  }
+  if (askMeaning) {
+    return <TestQuestion />;
+  } else {
+    return <TestConjugation />;
+  }
 }
